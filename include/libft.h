a0dfd8dd1f757a8f7880b128075aa793986225fc
drahwanj <deaarahwnji@gmail.com>
2025-02-14 20:03:39 +0300

    libft.h

---
+++ include/libft.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drahwanj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 19:30:52 by drahwanj          #+#    #+#             */
+/*   Updated: 2025/02/14 19:30:55 by drahwanj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include "minishell.h"
+
+/*
+Return the length of S
+*/
+int		ft_strlen(const char *s);
+/*
+Allocate COUNT elements of SIZE bytes each, all initialized to 0.
+*/
+void	*ft_calloc(size_t count, size_t size);
+/*
+Append SRC onto DEST.
+*/
+char	*ft_strcat(char *dest, const char *src);
+/*
+Finds the first occurence of C in S.
+*/
+char	*ft_strchr(const char *s, int c);
+
+#endif
