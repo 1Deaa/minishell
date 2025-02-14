c150cedcd589bf87360aae29f5c7386ea7d5dc2c
drahwanj <deaarahwnji@gmail.com>
2025-02-14 14:23:44 +0300

    function shell_loop()

---
+++ src/shell.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shell.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drahwanj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 14:17:40 by drahwanj          #+#    #+#             */
+/*   Updated: 2025/02/14 14:17:40 by drahwanj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	shell_loop(void)
+{
+	t_input	shell;
+
+	shell.prompt = create_prompt();
+	while (true)
+	{
+		shell.command = readline(shell.prompt);
+		if (!shell.command)
+		{
+			free(shell.prompt);
+			break ;
+		}
+		add_history(shell.command);
+		free(shell.command);
+	}
+	rl_clear_history();
+	exit(EXIT_SUCCESS);
+}
