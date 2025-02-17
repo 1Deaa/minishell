3ffedc0b63da752684d7399a7c783f7d491a75eb
drahwanj <deaarahwnji@gmail.com>
2025-02-17 15:58:57 +0300

    Norminette Check Shell Script

---
+++ check_norminette.sh
@@ -0,0 +1,49 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    check_norminette.sh                                :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: drahwanj <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/02/17 15:53:45 by drahwanj          #+#    #+#              #
+#    Updated: 2025/02/17 15:53:53 by drahwanj         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/bash
+
+# Flag to track if all files pass
+all_ok=true
+
+# Loop through all .c files in the src directory
+for file in src/*.c; do
+    result=$(norminette $file)
+
+    if [[ "$result" == *"OK!"* ]]; then
+        echo "$file: OK!"
+    else
+        echo "$file: ERROR!"
+        all_ok=false
+    fi
+done
+
+# Loop through all .h files in the include directory
+for file in include/*.h; do
+    result=$(norminette $file)
+
+    if [[ "$result" == *"OK!"* ]]; then
+        echo "$file: OK!"
+    else
+        echo "$file: ERROR!"
+        all_ok=false
+    fi
+done
+
+# Return error code if any file failed
+if [ "$all_ok" = false ]; then
+    echo "Some files did not pass Norminette checks!"
+    exit 1
+else
+    echo "All files passed Norminette checks!"
+    exit 0
+fi
