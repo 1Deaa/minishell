3c0c43e750fff316ff03327bb9308d89177d0345
drahwanj <deaarahwnji@gmail.com>
2025-05-03 17:56:11 +0300

    update(echo): builtin echo now works as intended

--- src/bn_echo.c
+++ src/bn_echo.c
@@ -12,17 +12,46 @@
 
 #include "minishell.h"
 
+static int	count_cmp(char *s, char c)
+{
+	int	count;
+
+	if (!s)
+		return (0);
+	count = 0;
+	while (*s)
+	{
+		if (*s == c)
+			count++;
+		s++;
+	}
+	return (count);
+}
+
 int	echo(t_pak *cmd)
 {
+	bool	newline;
+	int		i[2];
 	char	**argv;
-	int		i;
+	int		ret;
 
+	i[0] = 0;
+	i[1] = 0;
+	newline = true;
 	argv = cmd->full_cmd;
-	i = 1;
-	while (argv && argv[i])
+	while (argv && argv[++i[0]])
 	{
-		ft_printf(1, "%s\n", argv[i]);
-		i++;
+		if (!i[1] && !ft_strncmp(argv[i[0]], "-n", 2) && \
+			(count_cmp(argv[i[0]], 'n') == (ft_strlen(argv[i[0]]) - 1)))
+			newline = false;
+		else
+		{
+			i[1] = 1;
+			ft_printf(1, argv[i[0]]);
+			if (argv[i[0] + 1])
+				ft_printf(1, " ");
+		}
 	}
-	return (0);
+	ret = write(1, "\n", newline);
+	return (ret == -1);
 }
