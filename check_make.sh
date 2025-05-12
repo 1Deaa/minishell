2d9d3d0f1dfe86d3841241e912e4c238971f0cfb
drahwanj <deaarahwnji@gmail.com>
2025-05-12 23:23:01 +0300

    updated check_make code and removed libftprintf.a

--- check_make.sh
+++ check_make.sh
@@ -6,8 +6,10 @@ make
 # Check if make succeeded
 if [ $? -ne 0 ]; then
     echo "Make failed. Exiting with error code."
+    make fclean
     exit 1
 else
     echo "Make succeeded."
+    make fclean
     exit 0
 fi
